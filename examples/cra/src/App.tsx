acebd04d4fd36e3a8169ff93c91efb91c8da77f2
Benedikt Rötsch <opensource@axe312.dev>
2020-11-25 17:39:54 +0100

    feat: add emulated page view tracking

--- examples/cra/src/App.tsx
+++ examples/cra/src/App.tsx
@@ -1,62 +1,58 @@
-import React from "react";
+import React from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
-} from "react-router-dom";
-import YouTube from "react-youtube";
+  Link,
+  useLocation,
+} from 'react-router-dom'
+import YouTube from 'react-youtube'
+import './App.css'
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
+// Implementation according to react-router docs:
+// https://reactrouter.com/web/api/Hooks/uselocation
 
-import "./App.css"
+function usePageViews() {
+  let location = useLocation()
+  React.useEffect(() => {
+    console.log(
+      'emulated page track event for',
+      location.pathname + location.search
+    )
+  }, [location])
+}
 
-export default function BasicExample() {
+function BasicExample() {
+  usePageViews()
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
 
-        <hr />
+      <hr />
 
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </>
+  )
 }
 
 // You can think of these components as "pages"
@@ -67,16 +63,16 @@ function Home() {
     <div>
       <h2>Home</h2>
     </div>
-  );
+  )
 }
 
 function About() {
   return (
     <div>
       <h2>About</h2>
-      <YouTube id="fooodQw4w9WgXcQ"/>
+      <YouTube id="fooodQw4w9WgXcQ" />
     </div>
-  );
+  )
 }
 
 function Dashboard() {
@@ -84,5 +80,14 @@ function Dashboard() {
     <div>
       <h2>Dashboard</h2>
     </div>
-  );
+  )
 }
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <BasicExample />
+    </Router>
+  )
+}
+export default App
