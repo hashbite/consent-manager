c6bc7355e6913c66ea9b071d5d4ac12aa6718b1c
Stephan Schneider <stephanschndr@gmail.com>
2020-12-04 18:30:19 +0100

    wip: add parcel example

--- examples/cra/babel-plugin-macros.config.js
+++ examples/cra/babel-plugin-macros.config.js
@@ -1,17 +1,5 @@
 module.exports = {
   twin: {
-    styled: {
-      import: 'default',
-      from: '@emotion/styled'
-    },
-    css: {
-      import: 'css',
-      from: '@emotion/react'
-    },
-    global: {
-      import: 'Global',
-      from: '@emotion/react'
-    },
     config: 'tailwind.config.js',
     debugProp: true,
     debugPlugins: false,
