9da982a365edbf383efc4da05b72ecf67d0a887c
Stephan Schneider <stephanschndr@gmail.com>
2020-12-04 19:23:33 +0100

    refactor: remove examples

--- examples/cra/babel-plugin-macros.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  twin: {
-    config: 'tailwind.config.js',
-    debugProp: true,
-    debugPlugins: false,
-    debug: true
-  }
-}
-
--- examples/cra/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import './index.css'
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './i18n'
-import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
