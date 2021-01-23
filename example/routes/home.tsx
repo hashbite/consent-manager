7384e14cd4fa304b002a82c983fdd1929bfc904a
Benedikt Rötsch <opensource@axe312.dev>
2021-01-23 17:54:15 +0100

    feat(example): add pseudo event tracking

--- example/routes/home.tsx
+++ example/routes/home.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react'
+import { useRedBoxLtd } from '../integrations/tracker-red-box-ltd'
 
 export default function RouteHome() {
+  const { trackEvent, isEnabled } = useRedBoxLtd()
+
   return (
     <>
       <h1>Welcome to the example page</h1>
-      <p>This page has no real content on purpose.</p>
-      <p>Switch to the video subroute to see the privacy shield in action.</p>
+      <p>
+        The home page demonstrates integrations that gather statistics about the
+        user.
+      </p>
+      <p>
+        Switch to the video subroute to see the social media privacy shield in
+        action.
+      </p>
+      <button onClick={() => trackEvent('click', 'watching', 'you', 'closely')}>
+        Do not click here -{' '}
+        {isEnabled ? 'we will track you' : 'nothing will happen'}
+      </button>
     </>
   )
 }
