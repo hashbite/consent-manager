f4cdda77c9c70c76e9debf9a91b9d42779b0218e
Benedikt Rötsch <opensource@axe312.dev>
2021-03-21 11:28:28 +0100

    feat: slide toggle button out of viewport when form opens

--- packages/interface-default/src/toggle-button.tsx
+++ packages/interface-default/src/toggle-button.tsx
@@ -1,28 +1,44 @@
 import React from 'react'
 import clsx from 'clsx'
+import { CSSTransition } from 'react-transition-group'
 
 import { Styles, ToggleIconProps } from './index'
 
+import defaultAnimationStyles from './animation-slide-out.module.css'
+
 export interface ToggleButtonProps {
   styles: Styles
+  animationStyles?: Styles
   ToggleIcon: React.ComponentType<ToggleIconProps>
   showForm: boolean
   toggleControlForm: Function
+  slideDuration?: number
 }
 
 export const ToggleButton: React.FC<ToggleButtonProps> = ({
   styles,
+  animationStyles = defaultAnimationStyles,
   ToggleIcon,
   showForm,
   toggleControlForm,
+  slideDuration = 700,
 }) => (
-  <button
-    className={clsx(styles.toggleButton)}
-    title={`Toggle website settings visibility`}
-    onClick={e => toggleControlForm(e)}
+  <CSSTransition
+    in={showForm}
+    timeout={slideDuration}
+    classNames={animationStyles}
   >
-    <div className={clsx(styles.pane, styles.toggleButtonContent)}>
-      <ToggleIcon className={clsx(showForm && styles.inverted)} />
-    </div>
-  </button>
+    <button
+      className={clsx(styles.toggleButton)}
+      title={`Toggle website settings visibility`}
+      onClick={e => toggleControlForm(e)}
+      style={{
+        transitionDuration: `${slideDuration}ms`,
+      }}
+    >
+      <div className={clsx(styles.pane, styles.toggleButtonContent)}>
+        <ToggleIcon className={clsx(showForm && styles.inverted)} />
+      </div>
+    </button>
+  </CSSTransition>
 )
