552706029fc91f7b4489f37f16f526e7d8d87dc7
Benedikt Rötsch <opensource@axe312.dev>
2021-03-26 12:13:15 +0100

    feat: add vimeo integration

---
+++ packages/integration-vimeo/src/index.ts
@@ -0,0 +1,28 @@
+import {
+  createIconComponentFromSimpleIconsSvgPath,
+  getForegroundColor,
+  IntegrationConfig,
+} from '@consent-manager/core'
+
+import Vimeo from 'simple-icons/icons/vimeo'
+
+export { default as VimeoVideo } from './vimeo-video'
+
+export function vimeoIntegration(): IntegrationConfig {
+  const { title, slug, hex, path } = Vimeo
+  const color = `#${hex}`
+  const contrastColor = getForegroundColor(color)
+  const Icon = createIconComponentFromSimpleIconsSvgPath(title, path)
+
+  return {
+    id: slug,
+    title,
+    category: 'social',
+    color,
+    contrastColor,
+    Icon,
+    privacyPolicyUrl: `https://vimeo.com/privacy`,
+    description:
+      'Vimeo is a video sharing service trusted By Over 80 Million Worldwide.',
+  }
+}
