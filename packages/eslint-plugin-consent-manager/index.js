73c016a39756a437d4ed9501fcec6af3f4cef7b1
Benedikt Rötsch <opensource@axe312.dev>
2021-04-06 16:54:31 +0200

    fixup! feat(eslint): introduce new consent-manager eslint-plugin

--- packages/eslint-plugin-consent-manager/index.js
+++ packages/eslint-plugin-consent-manager/index.js
@@ -3,8 +3,7 @@ module.exports = {
     'do-not-inject-scripts': {
       meta: {
         docs: {
-          description:
-            'Return the script tags instead of injecting them yourself. That way they can be removed when the user revokes consent.',
+          description: 'Our own useScript() hook should be used.',
         },
         schema: [],
       },
@@ -21,8 +20,11 @@ module.exports = {
             ) {
               context.report({
                 node,
-                message:
-                  'Return the script tags instead of injecting them yourself. That way they can be removed when the user revokes consent.',
+                message: [
+                  'Do not create script tags on your own. You might break the consent detection and revocation.',
+                  "import { useScript } from '@consent-manager/core'",
+                  "useScript('//foo.bar/tracking.js')",
+                ].join('\n'),
               })
             }
           },
