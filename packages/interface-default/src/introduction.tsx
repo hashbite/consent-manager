3882a52943b48d421ecd6e28585bb180f5bb6e13
Benedikt Rötsch <opensource@axe312.dev>
2021-04-14 19:02:59 +0200

    refactor: make introduction persistent till user opens form, enabels all or closes

--- packages/interface-default/src/introduction.tsx
+++ packages/interface-default/src/introduction.tsx
@@ -1,19 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import clsx from 'clsx'
 import { CSSTransition } from 'react-transition-group'
 import createActivityDetector from 'activity-detector-ssr'
 import { Trans } from '@lingui/react'
+
+import { useDecisions } from '@consent-manager/core'
+
+import { Styles } from './index'
 import defaultStyles from './index.module.css'
 import defaultAnimationStyles from './animation-slide.module.css'
-import { Styles } from './index'
 
 export interface IntroductionProps {
   introductionFinished: Function
+  setShowForm: Function
   styles?: Styles
   animationStyles?: Styles
   slideDuration: number
   noActionDelay?: number
-  visibleDuration?: number
 }
 
 interface ActivityDetector {
@@ -26,9 +29,10 @@ export const Introduction: React.FC<IntroductionProps> = ({
   styles = defaultStyles,
   animationStyles = defaultAnimationStyles,
   slideDuration,
-  noActionDelay = 4000,
-  visibleDuration = 6000,
+  setShowForm,
+  noActionDelay = 100,
 }) => {
+  const [decisions, setAndStoreDecisions] = useDecisions()
   const [show, setShow] = useState(false)
   const [isIdle, setIsIdle] = React.useState(false)
   const [activityDetectorInstance, setActivityDetector] = useState<
@@ -56,6 +60,35 @@ export const Introduction: React.FC<IntroductionProps> = ({
     }
   }, [isIdle, setShow, activityDetectorInstance])
 
+  const onLearnMore = useCallback(
+    e => {
+      e.preventDefault()
+      setShow(false)
+      setShowForm(true)
+    },
+    [setShow, setShowForm]
+  )
+
+  const onEnableAll = useCallback(
+    e => {
+      e.preventDefault()
+      setShow(false)
+      for (const integrationId of Object.keys(decisions)) {
+        decisions[integrationId] = true
+      }
+      setAndStoreDecisions(decisions)
+    },
+    [setShow, decisions, setAndStoreDecisions]
+  )
+
+  const onClose = useCallback(
+    e => {
+      e.preventDefault()
+      setShow(false)
+    },
+    [setShow]
+  )
+
   return (
     <CSSTransition
       in={show}
@@ -63,24 +96,54 @@ export const Introduction: React.FC<IntroductionProps> = ({
       classNames={animationStyles}
       mountOnEnter
       unmountOnExit
-      onEntering={() => {
-        window.setTimeout(() => {
-          setShow(false)
-        }, visibleDuration)
-      }}
       onExited={() => introductionFinished()}
     >
-      <div
+      <section
         className={clsx(styles.pane, styles.slide)}
         style={{ transitionDuration: `${slideDuration}ms` }}
       >
         <div className={clsx(styles.introduction, styles.content)}>
+          <h1 className={clsx(styles.headline)}>
+            <Trans
+              id="consent-manager.introduction.title"
+              message="Data protection enabled."
+            />
+          </h1>
+          <p className={clsx(styles.description)}>
+            <Trans
+              id="consent-manager.introduction.description"
+              message="Some features were disabled to protect your privacy."
+            />
+          </p>
+          <div className={clsx(styles.controls)}>
+            <button className={clsx(styles.button)} onClick={onLearnMore}>
+              <Trans
+                id="consent-manager.introduction.learn-more"
+                message="Learn more"
+              />
+            </button>
+            <button className={clsx(styles.button)} onClick={onEnableAll}>
+              <Trans
+                id="consent-manager.introduction.enable-all"
+                message="Enable all features"
+              />
+            </button>
+          </div>
           <Trans
-            id="consent-manager.introduction"
-            message="Some features got disabled in respect of your privacy."
+            id="consent-manager.introduction.close"
+            message="close"
+            render={({ translation }) => (
+              <button
+                className={clsx(styles.close)}
+                onClick={onClose}
+                title={String(translation)}
+              >
+                x
+              </button>
+            )}
           />
         </div>
-      </div>
+      </section>
     </CSSTransition>
   )
 }
