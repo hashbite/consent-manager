853d220b94019c53c22866cfb8a66a4af54f2380
Benedikt Rötsch <opensource@axe312.dev>
2021-04-16 13:14:10 +0200

    fix: close button no more triggers form save

--- packages/interface-default/src/form.tsx
+++ packages/interface-default/src/form.tsx
@@ -67,112 +67,120 @@ export const ConsentForm: React.FC<ConsentFormProps> = ({
   const onClose = useCallback(() => setShowForm(false), [setShowForm])
 
   return (
-    <Form
-      onSubmit={onSubmitCb}
-      initialValues={initialState}
-      render={({ handleSubmit, form }) => {
-        const controls = (
-          <div className={clsx(styles.formControls)}>
-            <SubmitButton
-              type="button"
-              onClick={form.reset}
-              className={clsx(styles.buttonReset, styles.button)}
-            >
-              <Trans id="consent-manager.form.reset" message="Reset" />
-            </SubmitButton>
-            <SubmitButton
-              type="button"
-              onClick={() =>
-                form.batch(() => {
-                  for (const id of Object.keys(initialState)) {
-                    form.change(id, false)
-                  }
-                })
-              }
-              className={clsx(styles.buttonReset, styles.button)}
-            >
-              <Trans
-                id="consent-manager.form.disableAll"
-                message="Disable all"
-              />
-            </SubmitButton>
-            <SubmitButton
-              type="button"
-              onClick={() =>
-                form.batch(() => {
-                  for (const id of Object.keys(initialState)) {
-                    form.change(id, true)
-                  }
-                })
-              }
-              className={clsx(styles.buttonReset, styles.button)}
-            >
-              <Trans id="consent-manager.form.enableAll" message="Enable all" />
-            </SubmitButton>
-            <SubmitButton
-              type="submit"
-              className={clsx(
-                styles.buttonReset,
-                styles.button,
-                styles.buttonPrimary
-              )}
-            >
-              <Trans id="consent-manager.form.save" message="Close and save" />
-            </SubmitButton>
-          </div>
-        )
-
-        return (
-          <form onSubmit={handleSubmit}>
-            <div className={clsx(styles.formIntro)}>
-              <div className={clsx(styles.formContent)}>
-                <h1 className={clsx(styles.formTitle)}>
-                  <Trans
-                    id="consent-manager.form.headline"
-                    message="Website Features and Cookies"
-                  />
-                </h1>
+    <>
+      <Form
+        onSubmit={onSubmitCb}
+        initialValues={initialState}
+        render={({ handleSubmit, form }) => {
+          const controls = (
+            <div className={clsx(styles.formControls)}>
+              <SubmitButton
+                type="button"
+                onClick={form.reset}
+                className={clsx(styles.buttonReset, styles.button)}
+              >
+                <Trans id="consent-manager.form.reset" message="Reset" />
+              </SubmitButton>
+              <SubmitButton
+                type="button"
+                onClick={() =>
+                  form.batch(() => {
+                    for (const id of Object.keys(initialState)) {
+                      form.change(id, false)
+                    }
+                  })
+                }
+                className={clsx(styles.buttonReset, styles.button)}
+              >
                 <Trans
-                  id="consent-manager.form.description"
-                  message="<0>Some features are disabled by default Third Party Services are disabled to protect your privacy.</0><1>To fully experience this website enable the following features:</1>"
-                  components={[<p />, <p />, <p />, <p />, <p />]}
+                  id="consent-manager.form.disableAll"
+                  message="Disable all"
                 />
-              </div>
+              </SubmitButton>
+              <SubmitButton
+                type="button"
+                onClick={() =>
+                  form.batch(() => {
+                    for (const id of Object.keys(initialState)) {
+                      form.change(id, true)
+                    }
+                  })
+                }
+                className={clsx(styles.buttonReset, styles.button)}
+              >
+                <Trans
+                  id="consent-manager.form.enableAll"
+                  message="Enable all"
+                />
+              </SubmitButton>
+              <SubmitButton
+                type="submit"
+                className={clsx(
+                  styles.buttonReset,
+                  styles.button,
+                  styles.buttonPrimary
+                )}
+              >
+                <Trans
+                  id="consent-manager.form.save"
+                  message="Close and save"
+                />
+              </SubmitButton>
             </div>
-            {controls}
-            <div className={clsx(styles.formIntegrations)}>
-              <div className={clsx(styles.formContent)}>
-                {integrations.map((integration: IntegrationConfigOptions) => (
-                  <Integration
-                    styles={styles}
-                    key={integration.id}
-                    Switch={Switch}
-                    {...integration}
+          )
+
+          return (
+            <form onSubmit={handleSubmit}>
+              <div className={clsx(styles.formIntro)}>
+                <div className={clsx(styles.formContent)}>
+                  <h1 className={clsx(styles.formTitle)}>
+                    <Trans
+                      id="consent-manager.form.headline"
+                      message="Website Features and Cookies"
+                    />
+                  </h1>
+                  <Trans
+                    id="consent-manager.form.description"
+                    message="<0>Some features are disabled by default Third Party Services are disabled to protect your privacy.</0><1>To fully experience this website enable the following features:</1>"
+                    components={[<p />, <p />, <p />, <p />, <p />]}
                   />
-                ))}
+                </div>
               </div>
-            </div>
-            {controls}
-            <Trans
-              id="consent-manager.close"
-              message="close"
-              render={({ translation }) => (
-                <button
-                  className={clsx(
-                    styles.buttonReset,
-                    styles.buttonClose,
-                    styles.formButtonClose
-                  )}
-                  onClick={onClose}
-                  title={String(translation)}
-                >
-                  <CloseIcon className={clsx(styles.buttonCloseIcon)} />
-                </button>
-              )}
-            />
-          </form>
-        )
-      }}
-    />
+              {controls}
+              <div className={clsx(styles.formIntegrations)}>
+                <div className={clsx(styles.formContent)}>
+                  {integrations.map((integration: IntegrationConfigOptions) => (
+                    <Integration
+                      styles={styles}
+                      key={integration.id}
+                      Switch={Switch}
+                      {...integration}
+                    />
+                  ))}
+                </div>
+              </div>
+              {controls}
+            </form>
+          )
+        }}
+      />
+      <Trans
+        id="consent-manager.close"
+        message="close"
+        render={({ translation }) => (
+          <button
+            className={clsx(
+              styles.buttonReset,
+              styles.buttonClose,
+              styles.formButtonClose
+            )}
+            onClick={onClose}
+            title={String(translation)}
+          >
+            <CloseIcon className={clsx(styles.buttonCloseIcon)} />
+          </button>
+        )}
+      />
+    </>
   )
 }
